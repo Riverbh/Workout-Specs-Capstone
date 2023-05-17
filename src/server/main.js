4087ed4e9427f13abc55035a0d6d6e66e1a72c04
River Hansen <riverbh01@gmail.com>
2023-05-17 14:59:26 -0600

    created the data tables in models.js and associations in main.js

--- src/server/main.js
+++ src/server/main.js
@@ -2,8 +2,23 @@ const express = require("express");
 const ViteExpress = require("vite-express");
 const {login, register} = require('./controllers/auth')
 const db = require('./util/db')
+const {User, Post, Like, Goal} = require('./util/models')
 
 const app = express();
+app.use(express.json())
+
+//! associations 
+User.hasMany(Post)
+Post.belongsTo(User)
+
+Like.belongsTo(Post)
+Post.hasMany(Like)
+
+Like.belongsTo(User)
+User.hasMany(Like)
+
+User.hasMany(Goal)
+Goal.belongsTo(User)
 
 app.post('/api/register', register)
 app.post('/api/login', login)
--- src/server/util/models.js
+++ src/server/util/models.js
@@ -2,5 +2,64 @@ const db = require('./db')
 const {DataTypes} = require('sequelize')
 
 module.exports = {
-    
+    User: db.define("user", {
+        id: {
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+            type: DataTypes.INTEGER
+        },
+        username: {
+            type: DataTypes.STRING({length:20}),
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING({length:300}),
+            allowNull: false
+        },
+    }),
+    Post: db.define("post", {
+        id: {
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+            type: DataTypes.INTEGER
+        },
+        title: {
+            type: DataTypes.STRING({length: 40}),
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING({length: 300}),
+            allowNull: false,
+        }
+    }),
+    Like: db.define("like", {
+        id: {
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+            type: DataTypes.INTEGER
+        },
+        like: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        }
+    }),
+    Goal: db.define("goal", {
+        id: {
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+            type: DataTypes.INTEGER
+        },
+        description: {
+            type: DataTypes.STRING({length: 300}),
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        }
+    }),
 }
